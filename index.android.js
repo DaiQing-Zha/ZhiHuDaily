47f7fd9ded967e27583cd3ddf20f662ff5b7979f
DaiQing.Zha <13767191284@163.com>
2017-09-01 17:52:52 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './react/index'
+
+AppRegistry.registerComponent('ZhiHuDailyRN', () => App);
